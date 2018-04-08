0e760fdcd93d224284213bb797a9f90cd2ccaa93
Tomasz Prus <tomasz.prus@gmail.com>
2018-04-08 02:24:10 +0200

    chore: add asynchronous examples

---
+++ examples/example1.py
@@ -0,0 +1,24 @@
+from kubernetes import client, config
+import asyncio
+
+
+async def main():
+    # Configs can be set in Configuration class directly or using helper
+    # utility. If no argument provided, the config will be loaded from
+    # default location.
+    config.load_kube_config()
+
+    v1 = client.CoreV1Api()
+    print("Listing pods with their IPs:")
+    ret = await v1.list_pod_for_all_namespaces(watch=False)
+
+    for i in ret.items:
+        print("%s\t%s\t%s" %
+              (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    loop.close()
+
