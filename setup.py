a697a154b4b856b10473c0a264f09a7f8e470937
Tomasz Prus <tomasz.prus@gmail.com>
2018-04-08 02:21:20 +0200

    chore: update setup.py and test files

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import find_packages, setup
 # Do not edit these constants. They will be updated automatically
 # by scripts/update-client.sh.
 CLIENT_VERSION = "6.0.0-snapshot"
-PACKAGE_NAME = "kubernetes"
+PACKAGE_NAME = "kubernetes-asyncio"
 DEVELOPMENT_STATUS = "4 - Beta"
 
 # To install the library, run the following
