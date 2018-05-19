b112c02b324404a2c4a3f73db44a9a31e5956f65
Tomasz Prus <tomasz.prus@gmail.com>
2018-05-20 00:53:30 +0200

    doc: update readme

--- README.md
+++ README.md
@@ -44,4 +44,4 @@ if __name__ == '__main__':
     loop.close()
 ```
 
-More examples you can find in `example/` folder.
+More examples you can find in `examples/` folder.
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import find_packages, setup
 
 # Do not edit these constants. They will be updated automatically
 # by scripts/update-client.sh.
-CLIENT_VERSION = "6.0.0-snapshot"
+CLIENT_VERSION = "1.0.0-alpha1"
 PACKAGE_NAME = "kubernetes_asyncio"
 DEVELOPMENT_STATUS = "2 - Pre-Alpha"
 
