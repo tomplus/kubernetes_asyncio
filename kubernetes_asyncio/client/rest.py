46c0864f815072605feca8d681173a0e7312b89a
Tomasz Prus <tomasz.prus@gmail.com>
2018-05-26 02:27:23 +0200

    feat: support for strategic-merge-patch+json

--- kubernetes_asyncio/client/rest.py
+++ kubernetes_asyncio/client/rest.py
@@ -133,6 +133,9 @@ class RESTClientObject(object):
         # For `POST`, `PUT`, `PATCH`, `OPTIONS`, `DELETE`
         if method in ['POST', 'PUT', 'PATCH', 'OPTIONS', 'DELETE']:
             if re.search('json', headers['Content-Type'], re.IGNORECASE):
+                if headers['Content-Type'] == 'application/json-patch+json':
+                    if not isinstance(body, list):
+                        headers['Content-Type'] = 'application/strategic-merge-patch+json'
                 if body is not None:
                     body = json.dumps(body)
                 args["data"] = body
