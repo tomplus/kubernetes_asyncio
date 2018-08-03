72221b29528c65349c0790e443bb64240cb50401
Tomasz Prus <tomasz.prus@gmail.com>
2018-08-03 07:58:57 +0200

    chore: update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup
 
 # Do not edit these constants. They will be updated automatically
 # by scripts/update-client.sh.
-CLIENT_VERSION = "1.0.0-beta4"
+CLIENT_VERSION = "1.0.0-beta5"
 PACKAGE_NAME = "kubernetes_asyncio"
 DEVELOPMENT_STATUS = "4 - Beta"
 
