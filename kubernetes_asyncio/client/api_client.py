6b50f01f282278a86fd6605cbb2f5fa8b549a3f5
Tomasz Prus <tomasz.prus@gmail.com>
2018-11-11 00:50:01 +0100

    feat: regenerate client

--- kubernetes_asyncio/client/api_client.py
+++ kubernetes_asyncio/client/api_client.py
@@ -73,7 +73,7 @@ class ApiClient(object):
             self.default_headers[header_name] = header_value
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'Swagger-Codegen/8.0.0-snapshot/python'
+        self.user_agent = 'Swagger-Codegen/8.0.0/python'
 
     def __del__(self):
         self.pool.close()
