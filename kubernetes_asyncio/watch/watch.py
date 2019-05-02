0a1a0f06ad35b338f024d1d5e766a05f450a40f3
MERCIER Michael <michael.mercier@inria.fr>
2019-05-02 10:25:23 +0200

    Also add check on the 'type' field on watch object

--- kubernetes_asyncio/watch/watch.py
+++ kubernetes_asyncio/watch/watch.py
@@ -70,8 +70,8 @@ class Watch(object):
         """
         js = json.loads(data)
 
-        if 'object' not in js:
-            raise Exception("Malformed JSON response, 'object' field is missing. JSON: {}".format(js))
+        if 'object' not in js or 'type' not in js:
+            raise Exception("Malformed JSON response, the 'object' and/or 'type' field is missing. JSON: {}".format(js))
         # Make a copy of the original object and save it under the
         # `raw_object` key because we will replace the data under `object` with
         # a Python native type shortly.
