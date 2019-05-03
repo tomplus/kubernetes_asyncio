789be984f95fc8f0aa1776a7ed0ebc7ce7de1849
Tomasz Prus <tomasz.prus@gmail.com>
2019-05-03 23:14:14 +0200

    chore: bump version

--- kubernetes_asyncio/client/configuration.py
+++ kubernetes_asyncio/client/configuration.py
@@ -231,5 +231,5 @@ class Configuration(six.with_metaclass(TypeWithDefault, object)):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: v1.13.5\n"\
-               "SDK Package Version: 9.0.0".\
+               "SDK Package Version: 9.0.1".\
                format(env=sys.platform, pyversion=sys.version)
