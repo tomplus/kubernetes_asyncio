a4a95f6a7c0320f164472a53204cd4eba2eac527
Tomasz Prus <tomasz.prus@gmail.com>
2020-10-23 00:40:14 +0200

    chore: bump version

--- kubernetes_asyncio/client/configuration.py
+++ kubernetes_asyncio/client/configuration.py
@@ -348,7 +348,7 @@ conf = client.Configuration(
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: v1.16.14\n"\
-               "SDK Package Version: 12.0.0".\
+               "SDK Package Version: 12.0.1".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
