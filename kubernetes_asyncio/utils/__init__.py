2354b4d12a2ec81b093075506bf4643fc59accc1
VideoSystemsTech <61198558+VideoSystemsTech@users.noreply.github.com>
2021-02-15 01:15:28 +0100

    Missing create_from_yaml_single_item import (#133)

--- kubernetes_asyncio/utils/__init__.py
+++ kubernetes_asyncio/utils/__init__.py
@@ -14,4 +14,4 @@
 
 from __future__ import absolute_import
 
-from .create_from_yaml import FailToCreateError, create_from_yaml
+from .create_from_yaml import FailToCreateError, create_from_yaml, create_from_yaml_single_item
