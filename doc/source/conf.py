9a1543099be65c5e5e13fc21e359052cb7d3618b
Tomasz Prus <tomasz.prus@gmail.com>
2021-10-17 00:51:23 +0200

    fix: pylint errors

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -15,8 +15,6 @@
 import os
 import sys
 
-from recommonmark.parser import CommonMarkParser
-
 sys.path.insert(0, os.path.abspath('../..'))
 # -- General configuration ----------------------------------------------------
 
