5951bf9f4275ac20aff5a3caadbca17583490704
Tomasz Prus <tomasz.prus@gmail.com>
2021-10-16 23:55:04 +0200

    chore: run tests on Python 3.10 (#160)

--- setup.py
+++ setup.py
@@ -74,5 +74,6 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
 )
