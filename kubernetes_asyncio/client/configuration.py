f4e11b7223e546515e99c984f9948b6caa06622a
Tomasz Prus <tomasz.prus@gmail.com>
2021-12-11 00:44:21 +0100

    chore: update version

--- kubernetes_asyncio/client/configuration.py
+++ kubernetes_asyncio/client/configuration.py
@@ -348,7 +348,7 @@ conf = client.Configuration(
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: v1.19.15\n"\
-               "SDK Package Version: 19.15.0-snapshot".\
+               "SDK Package Version: 19.15.0".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
--- scripts/constants.py
+++ scripts/constants.py
@@ -18,7 +18,7 @@ import sys
 KUBERNETES_BRANCH = "v1.19.15"
 
 # client version for packaging and releasing.
-CLIENT_VERSION = "19.15.0-snapshot"
+CLIENT_VERSION = "19.15.0"
 
 # Name of the release package
 PACKAGE_NAME = "kubernetes_asyncio"
