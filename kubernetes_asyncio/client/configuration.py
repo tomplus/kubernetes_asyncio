fb7db292eb92719bdce2e5c159f90e0cd27c7170
Tomasz Prus <tomasz.prus@gmail.com>
2022-03-10 00:06:50 +0100

    [feat] release v20.13.0

--- kubernetes_asyncio/client/configuration.py
+++ kubernetes_asyncio/client/configuration.py
@@ -348,7 +348,7 @@ conf = client.Configuration(
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: v1.20.13\n"\
-               "SDK Package Version: 20.13.0-snapshot".\
+               "SDK Package Version: 20.13.0".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
