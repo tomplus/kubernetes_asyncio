b331377ba485ab9d50f508fc11a2b7525fc77303
Tomasz Prus <tomasz.prus@gmail.com>
2022-03-13 01:16:41 +0100

    [feat] regenerate librarty for API v1.22.6

--- scripts/constants.py
+++ scripts/constants.py
@@ -15,10 +15,10 @@
 import sys
 
 # Kubernetes branch to get the OpenAPI spec from.
-KUBERNETES_BRANCH = "v1.21.7"
+KUBERNETES_BRANCH = "v1.22.6"
 
 # client version for packaging and releasing.
-CLIENT_VERSION = "21.7.0-snapshot"
+CLIENT_VERSION = "22.6.0-snapshot"
 
 # Name of the release package
 PACKAGE_NAME = "kubernetes_asyncio"
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup
 
 # Do not edit these constants. They will be updated automatically
 # by scripts/update-client.sh.
-CLIENT_VERSION = "21.7.0-snapshot"
+CLIENT_VERSION = "22.6.0-snapshot"
 PACKAGE_NAME = "kubernetes_asyncio"
 DEVELOPMENT_STATUS = "4 - Beta"
 
