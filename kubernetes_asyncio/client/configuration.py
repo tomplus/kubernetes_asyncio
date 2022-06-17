3640de349f9fa26e009c6cd102c062b066fefe6b
Tomasz Prus <tomasz.prus@gmail.com>
2022-06-18 01:33:51 +0200

    [chore] update version

--- kubernetes_asyncio/client/configuration.py
+++ kubernetes_asyncio/client/configuration.py
@@ -406,7 +406,7 @@ conf = client.Configuration(
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: v1.23.6\n"\
-               "SDK Package Version: 23.6.0-snapshot".\
+               "SDK Package Version: 23.6.0".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
