7bbb327cee0d4ed908a5deb28aaf5a9ccc1f4602
Tomasz Prus <tomasz.prus@gmail.com>
2022-08-17 23:46:20 +0200

    [chore] update version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup
 
 # Do not edit these constants. They will be updated automatically
 # by scripts/update-client.sh.
-CLIENT_VERSION = "24.2.0"
+CLIENT_VERSION = "24.2.1"
 PACKAGE_NAME = "kubernetes_asyncio"
 DEVELOPMENT_STATUS = "5 - Production/Stable"
 
