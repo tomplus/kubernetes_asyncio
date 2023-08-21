9a41443088b999c00b6fbc7a88dcfbb86bf5c1c5
Tomasz Prus <tomasz.prus@gmail.com>
2023-08-21 13:58:21 +0200

    regenerate client for release 25.11.0 (#272)
    * Regenerate with K8s API v1.25.11
    
    * chore: update changelog
    
    * fix patch

--- scripts/constants.py
+++ scripts/constants.py
@@ -15,10 +15,10 @@
 import sys
 
 # Kubernetes branch to get the OpenAPI spec from.
-KUBERNETES_BRANCH = "v1.24.2"
+KUBERNETES_BRANCH = "v1.25.11"
 
 # client version for packaging and releasing.
-CLIENT_VERSION = "24.2.0+snapshot"
+CLIENT_VERSION = "25.11.0+snapshot"
 
 # Name of the release package
 PACKAGE_NAME = "kubernetes_asyncio"
