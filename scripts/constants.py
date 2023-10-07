7adc4ef866d3701cd48682dc02c46393f769aa2f
Tomasz Prus <tomasz.prus@gmail.com>
2023-10-08 00:26:49 +0200

    release v28.2.0

--- scripts/constants.py
+++ scripts/constants.py
@@ -18,7 +18,7 @@ import sys
 KUBERNETES_BRANCH = "v1.28.2"
 
 # client version for packaging and releasing.
-CLIENT_VERSION = "28.2.0+snapshot"
+CLIENT_VERSION = "28.2.0"
 
 # Name of the release package
 PACKAGE_NAME = "kubernetes_asyncio"
