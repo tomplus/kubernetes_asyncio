de49b1b1e52eb90da26fbfb13d1e61e3d206c3cb
Tomasz Prus <tomasz.prus@gmail.com>
2025-04-30 23:57:09 +0200

    [chore] bump version

--- kubernetes_asyncio/client/__init__.py
+++ kubernetes_asyncio/client/__init__.py
@@ -14,7 +14,7 @@
 
 from __future__ import absolute_import
 
-__version__ = "32.3.1"
+__version__ = "32.3.2"
 
 # import apis into sdk package
 from kubernetes_asyncio.client.api.well_known_api import WellKnownApi
--- kubernetes_asyncio/client/configuration.py
+++ kubernetes_asyncio/client/configuration.py
@@ -428,7 +428,7 @@ conf = client.Configuration(
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: v1.32.3\n"\
-               "SDK Package Version: 32.3.1".\
+               "SDK Package Version: 32.3.2".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
