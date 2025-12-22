babdabe2a5b9736194a4056d4370cccf2de1349b
Darshan Thakare <143271270+DarshanCode2005@users.noreply.github.com>
2025-12-22 14:07:40 +0530

    feat: support proxy-url from kubeconfig clusters and propagate to async client

--- kubernetes_asyncio/config/kube_config.py
+++ kubernetes_asyncio/config/kube_config.py
@@ -396,6 +396,8 @@ class KubeConfigLoader(object):
             self.verify_ssl = not self._cluster['insecure-skip-tls-verify']
         if 'tls-server-name' in self._cluster:
             self.tls_server_name = self._cluster['tls-server-name']
+        if 'proxy-url' in self._cluster:
+            self.proxy = self._cluster['proxy-url']
 
     def _set_config(self, client_configuration):
 
@@ -403,7 +405,8 @@ class KubeConfigLoader(object):
             client_configuration.api_key['BearerToken'] = self.token
 
         # copy these keys directly from self to configuration object
-        keys = ['host', 'ssl_ca_cert', 'cert_file', 'key_file', 'verify_ssl', 'tls_server_name']
+        keys = ['host', 'ssl_ca_cert', 'cert_file', 'key_file',
+                'verify_ssl', 'tls_server_name', 'proxy']
         for key in keys:
             if key in self.__dict__:
                 setattr(client_configuration, key, getattr(self, key))
