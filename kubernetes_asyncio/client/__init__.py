e88536cc174e7ddefac058e16c5cfea26967ee89
Tomasz Prus <tomasz.prus@gmail.com>
2026-01-11 15:39:15 +0100

    [chore] bump version

--- kubernetes_asyncio/client/__init__.py
+++ kubernetes_asyncio/client/__init__.py
@@ -14,7 +14,7 @@
 
 from __future__ import absolute_import
 
-__version__ = "34.3.0"
+__version__ = "34.3.1"
 
 # import apis into sdk package
 from kubernetes_asyncio.client.api.well_known_api import WellKnownApi
--- scripts/constants.py
+++ scripts/constants.py
@@ -18,7 +18,7 @@ import sys
 KUBERNETES_BRANCH = "v1.34.3"
 
 # client version for packaging and releasing.
-CLIENT_VERSION = "34.3.0"
+CLIENT_VERSION = "34.3.1"
 
 # Name of the release package
 PACKAGE_NAME = "kubernetes_asyncio"
