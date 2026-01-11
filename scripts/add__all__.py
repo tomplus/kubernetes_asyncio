69775a3302f6b0bc460a736fdb9d11d318d88087
Tomasz Prus <tomasz.prus@gmail.com>
2026-01-11 14:47:32 +0100

    chore: add __all__ for imported symbols

---
+++ scripts/add__all__.py
@@ -0,0 +1,27 @@
+import sys
+import ast
+
+filepath = sys.argv[1]
+
+with open(filepath, "r") as f:
+    content = f.read()
+
+tree = ast.parse(content)
+
+if "__all__" in content:
+    raise Exception("__all__ already exists")
+
+imports = []
+
+for node in ast.walk(tree):
+    if isinstance(node, ast.ImportFrom):
+        for alias in node.names:
+            imports.append(alias.name)
+
+all_section = "\n__all__ = [\n"
+for imp in imports:
+    all_section += f'    "{imp}",\n'
+all_section += "]\n"
+
+with open(filepath, "a") as f:
+    f.write(all_section)
