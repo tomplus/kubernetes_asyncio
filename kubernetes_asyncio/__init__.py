452d31254e39a1f9ec38bd6d9226de2bc0ff92a9
Tomasz Prus <tomasz.prus@gmail.com>
2026-02-25 21:32:19 +0100

    [chore] bump version

--- kubernetes_asyncio/__init__.py
+++ kubernetes_asyncio/__init__.py
@@ -14,7 +14,7 @@
 
 __project__ = "kubernetes_asyncio"
 # The version is auto-updated. Please do not edit.
-__version__ = "35.0.0"
+__version__ = "35.0.1"
 
 import kubernetes_asyncio.client as client
 import kubernetes_asyncio.config as config
